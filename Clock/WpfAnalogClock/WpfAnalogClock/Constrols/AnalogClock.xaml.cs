c5820339de2aaa1f1677b4daec032e53aa84cd20
zbarjevsky <35174493+zbarjevsky@users.noreply.github.com>
2018-02-06 06:38:13 -0600

    AnalogClock - created

---
+++ Clock/WpfAnalogClock/WpfAnalogClock/Constrols/AnalogClock.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAnalogClock.Constrols
+{
+    /// <summary>
+    /// Interaction logic for AnalogClock.xaml
+    /// </summary>
+    public partial class AnalogClock : UserControl
+    {
+        public AnalogClock()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+        }
+    }
+}
