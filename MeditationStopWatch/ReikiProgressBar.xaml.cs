aee7af746ee4c4ee8c5c81cc6cef6593488626ae
zbarjevsky <35174493+zbarjevsky@users.noreply.github.com>
2019-05-04 20:06:33 -0500

    MeditationStopWatch - added ticks to ReiKi progress

--- MeditationStopWatch/ReikiProgressBar.xaml.cs
+++ MeditationStopWatch/ReikiProgressBar.xaml.cs
@@ -9,6 +9,8 @@ using System.ComponentModel;
 using System.Windows.Resources;
 using System.IO;
 using MeditationStopWatch;
+using System.Runtime.CompilerServices;
+using System.Windows.Shapes;
 
 namespace Wizard
 {
@@ -24,8 +26,8 @@ namespace Wizard
         private bool m_bSoundPlayed;
         //private SoundPlayer m_SoundPlayer;
         private MCIPLayer m_SoundPlayer = new MCIPLayer();
-        private string m_sExePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        private Options m_Options;
+        private string m_sExePath = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        private Options _options;
 
         public ReikiProgressBar()
         {
@@ -38,29 +40,11 @@ namespace Wizard
             m_Timer.Elapsed += m_Timer_Elapsed;
         }
 
-        //public SoundPlayer SoundPlayer
-        //{
-        //    get
-        //    {
-        //        if (m_SoundPlayer == null)
-        //        {
-        //            ////string assName = Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().Location);
-        //            //string uri = string.Format(@"pack://application:,,,/{0}", @"Sounds/onhold.wav");
-        //            //var info = Application.GetResourceStream(new Uri(uri));
-        //            //if (info != null)
-        //            //{
-        //            //    m_SoundPlayer = new SoundPlayer(info.Stream);
-        //            //}
-        //        }
-        //        return m_SoundPlayer;
-        //    }
-        //}
-
         public void PlayDing()
         {
-            string sName = Path.Combine(m_sExePath, "Sounds", "ding.mp3");
+            string sName = System.IO.Path.Combine(m_sExePath, "Sounds", "ding.mp3");
             m_SoundPlayer.Play(sName, "ding");
-            m_SoundPlayer.SetVolume(m_Options.Volume);
+            m_SoundPlayer.SetVolume(_options.Volume);
         }
 
         private void UpdateTooltip(double secondsLeft)
@@ -69,11 +53,18 @@ namespace Wizard
             {
                 const string FMT = @"m\:ss"; // @"hh\:mm\:ss"
 
-                string interval = TimeSpan.FromSeconds(m_Options.ProgressInterval).ToString(FMT);
+                string interval = TimeSpan.FromSeconds(_options.ProgressInterval).ToString(FMT);
                 string value = TimeSpan.FromSeconds(secondsLeft).ToString(FMT);
 
-                this.ToolTip = string.Format("{0}: {1:0} of {2:0}",
-                    m_Options.BellAtTheEnd ? "On" : "Off", value, interval);
+                if (Max > 10)
+                {
+                    this.ToolTip = string.Format("Bell: {0}, Interval: {1:0} Time Left: {2:0}",
+                        _options.BellAtTheEnd ? "On" : "Off", interval, value);
+                }
+                else
+                {
+                    this.ToolTip = "None";
+                }
             }));
         }
 
@@ -86,16 +77,23 @@ namespace Wizard
                 m_ElapsedTime += (now - m_LastTime);
             m_LastTime = now;
 
-			Value = m_ElapsedTime.TotalSeconds % Max;
-
-			if (m_Options.BellAtTheEnd && Max - Value < 2 && !m_bSoundPlayed)
+            if (Max > 10)
             {
-                m_bSoundPlayed = true;
-                PlayDing();
-            }
+                Value = m_ElapsedTime.TotalSeconds % Max;
 
-			if (Max - Value > 5)
-                m_bSoundPlayed = false;
+                if (_options.BellAtTheEnd && Max - Value < 2 && !m_bSoundPlayed)
+                {
+                    m_bSoundPlayed = true;
+                    PlayDing();
+                }
+
+                if (Max - Value > 5)
+                    m_bSoundPlayed = false;
+            }
+            else
+            {
+                Value = 0;
+            }
 
             UpdateTooltip(Max - Value);
 
@@ -106,21 +104,23 @@ namespace Wizard
         public double Value
         {
             get { return _value; }
-            set { _value = value;  OnPropertyChanged("Value"); }
+            set { _value = value;  OnPropertyChanged(); }
         }
 
 		public double Max
 		{
 			get 
 			{ 
-				if (m_Options == null) 
+				if (_options == null) 
 					return 180; 
-				return m_Options.ProgressInterval; 
+				return _options.ProgressInterval; 
 			}
 			set 
 			{ 
-				m_Options.ProgressInterval = value; 
-				OnPropertyChanged("Max"); 
+				_options.ProgressInterval = value;
+                DrawTicks();
+
+                OnPropertyChanged(); 
 			}
 		}
 
@@ -128,8 +128,8 @@ namespace Wizard
 		{
 			set 
 			{ 
-				m_Options = value;
-				chkBellOnOff.IsChecked = m_Options.BellAtTheEnd;
+				_options = value;
+				chkBellOnOff.IsChecked = _options.BellAtTheEnd;
 				InitInterval();
 				OnPropertyChanged("Max"); 
 			}
@@ -165,13 +165,12 @@ namespace Wizard
 
         #region INotifyPropertyChanged Members
 
+
         public event PropertyChangedEventHandler PropertyChanged;
-
-
-        protected void OnPropertyChanged(string name)
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             if (PropertyChanged != null)
-                PropertyChanged(this, new PropertyChangedEventArgs(name));
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         #endregion
@@ -189,7 +188,7 @@ namespace Wizard
 		private void OnBellOnOffClicked(object sender, RoutedEventArgs e)
 		{
 			var menu = e.Source as MenuItem;
-			m_Options.BellAtTheEnd = menu.IsChecked = !menu.IsChecked;
+			_options.BellAtTheEnd = menu.IsChecked = !menu.IsChecked;
 		}
 
 		private void OnBellIntervalClicked(object sender, RoutedEventArgs e)
@@ -199,8 +198,10 @@ namespace Wizard
 
 		private void SetInterval(MenuItem mnuClicked)
 		{
-			SetInterval(mnuClicked, chkBell2min, 120);
-			SetInterval(mnuClicked, chkBell3min, 180);
+            SetInterval(mnuClicked, chkNoProgress, 1);
+            SetInterval(mnuClicked, chkBell1min, 60);
+            SetInterval(mnuClicked, chkBell2min, 120);
+            SetInterval(mnuClicked, chkBell3min, 180);
 			SetInterval(mnuClicked, chkBell4min, 240);
 			SetInterval(mnuClicked, chkBell5min, 300);
 		}
@@ -220,16 +221,56 @@ namespace Wizard
 
 		private void InitInterval()
 		{
-			if (m_Options.ProgressInterval > 0 && m_Options.ProgressInterval < 121)
-				SetInterval(chkBell2min);
-			else if (m_Options.ProgressInterval > 120 && m_Options.ProgressInterval < 181)
+            if (_options.ProgressInterval < 31)
+                SetInterval(chkNoProgress);
+            else if (_options.ProgressInterval > 30 && _options.ProgressInterval < 61)
+                SetInterval(chkBell1min);
+            else if (_options.ProgressInterval > 60 && _options.ProgressInterval < 121)
+                SetInterval(chkBell2min);
+            else if (_options.ProgressInterval > 120 && _options.ProgressInterval < 181)
 				SetInterval(chkBell3min);
-			else if (m_Options.ProgressInterval > 180 && m_Options.ProgressInterval < 241)
+			else if (_options.ProgressInterval > 180 && _options.ProgressInterval < 241)
 				SetInterval(chkBell4min);
-			else if (m_Options.ProgressInterval > 240)
+			else if (_options.ProgressInterval > 240)
 				SetInterval(chkBell5min);
 			else //default
 				SetInterval(chkBell3min);
 		}
-	}
+
+        private void DrawTicks()
+        {
+            _canvas.Children.Clear();
+            double line_count = (Max / 30.0); //line per 30 sec
+            double line_offset = (_canvas.ActualWidth-3) / line_count;
+            double smallDelta = _canvas.ActualHeight / 10;
+            double bigDelta = _canvas.ActualHeight / 4;
+
+            for (int i = 0; i < line_count + 1; i++)
+            {
+                bool odd = i % 2 == 1;
+                double thickness = odd ? 1 : 2;
+                double delta = odd ? bigDelta : smallDelta;
+
+                Line line = new Line();
+                line.Stroke = System.Windows.Media.Brushes.DarkGoldenrod;
+                line.StrokeThickness = thickness;
+                line.X1 = 1 + i * line_offset;
+                line.X2 = line.X1;
+                line.Y1 = delta;
+                line.Y2 = _canvas.ActualHeight - delta;
+
+                _canvas.Children.Add(line);
+            }
+        }
+
+        private void ReiKi_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Start();
+        }
+
+        private void ReiKi_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            DrawTicks();
+        }
+    }
 }
