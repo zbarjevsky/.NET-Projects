faadc3ab33250c67d0d7b8148844a6235e043afe
mark.zbarjevsky <mark.zbarjevsky@ZBARJM1-TL1>
2019-12-24 03:51:56 -0600

    DashCamGPSView - adjusted TextBox style - with container size

--- DashCamGPSView/DashCamGPSView/Controls/PercentageConverter.cs
+++ DashCamGPSView/DashCamGPSView/Controls/PercentageConverter.cs
@@ -9,7 +9,7 @@ using System.Windows.Markup;
 
 namespace DashCamGPSView.Controls
 {
-    public class PercentageConverter : MarkupExtension, IValueConverter
+    public class PercentageConverter : MarkupExtension, IMultiValueConverter, IValueConverter
     {
         private static PercentageConverter _instance;
 
@@ -20,7 +20,25 @@ namespace DashCamGPSView.Controls
             return System.Convert.ToDouble(value) * System.Convert.ToDouble(parameter);
         }
 
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter is System.Double[] parameters)
+            {
+                double c1 = System.Convert.ToDouble(parameters[0]);
+                double c2 = System.Convert.ToDouble(parameters[1]);
+                double val1 = c1 * (double)values[0];
+                double val2 = c2 * (double)values[1];
+                return Math.Min(val1, val2);
+            }
+            return 16;
+        }
+
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
