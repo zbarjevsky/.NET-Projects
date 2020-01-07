96b6fa525ae647317a6710c81a8c42f3b2939863
mark.zbarjevsky <mark.zbarjevsky@ZBARJM1-TL1>
2020-01-07 08:42:37 -0600

    GPSDataParser - added simple JSON parser and IpInfo

---
+++ DashCamGPSView/GpsInfoParser/Tools/IpInfo.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using TinyJson;
+
+namespace GPSDataParser.Tools
+{
+    public class IpInfo
+    {
+        //[JsonProperty("ip")]
+        public string IP { get; set; }
+
+        //[JsonProperty("hostname")]
+        public string Hostname { get; set; }
+
+        //[JsonProperty("city")]
+        public string City { get; set; }
+
+        //[JsonProperty("region")]
+        public string Region { get; set; }
+
+        //[JsonProperty("country")]
+        public string Country { get; set; }
+
+        //[JsonProperty("loc")]
+        public string Loc { get; set; }
+
+        //[JsonProperty("org")]
+        public string Org { get; set; }
+
+        //[JsonProperty("postal")]
+        public string Postal { get; set; }
+
+        //[JsonProperty("Timezone")]
+        public string Timezone { get; set; }
+
+        public static IpInfo Parse(string json)
+        {
+            return json.FromJson<IpInfo>();
+        }
+
+        public static IpInfo Get()
+        {
+            string info = new WebClient().DownloadString("http://ipinfo.io");
+            return info.FromJson<IpInfo>();
+        }
+
+        public static string GetIpV4()
+        {
+            string externalipv4 = DownloadString("http://ipv4.icanhazip.com");
+            return externalipv4;
+        }
+
+        public static string GetIpV6()
+        {
+            string externalipv6 = DownloadString("http://ipv6.icanhazip.com");
+            return externalipv6;
+        }
+
+        public static string DownloadString(string address)
+        {
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    return client.DownloadString(address);
+                }
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
--- DashCamGPSView/TestWpfApp/App.xaml.cs
+++ DashCamGPSView/TestWpfApp/App.xaml.cs
@@ -12,6 +12,8 @@ using XLabs.Platform.Services.Geolocation;
 using System.Net;
 using System.IO;
 using System.Net.Sockets;
+using TinyJson;
+using GPSDataParser.Tools;
 
 namespace TestWpfApp
 {
@@ -36,98 +38,14 @@ namespace TestWpfApp
             string externalip = new WebClient().DownloadString("http://ipv4.icanhazip.com");
             string externalipv6 = new WebClient().DownloadString("http://ipv6.icanhazip.com");
 
-            //    string ip = GetLocalIPAddress();
-            //    if (!string.IsNullOrWhiteSpace(ip))
-            //        CityStateCountByIp(externalip.Trim());
-
             GetCountryByIP();
 
             Environment.Exit(-1);
         }
 
-        static void GetLocationProperty()
-        {
-
-            GeoCoordinateWatcher watcher = new GeoCoordinateWatcher();
-            if (watcher.Status == GeoPositionStatus.NoData)
-            {
-
-            }
-
-            // Do not suppress prompt, and wait 1000 milliseconds to start.
-            if (watcher.TryStart(false, TimeSpan.FromMilliseconds(10000)))
-            {
-                GeoCoordinate coord = watcher.Position.Location;
-                if (coord.IsUnknown != true)
-                {
-                    Console.WriteLine("Lat: {0}, Long: {1}",
-                        coord.Latitude,
-                        coord.Longitude);
-                }
-                else
-                {
-                    Console.WriteLine("Unknown latitude and longitude.");
-                }
-            }
-
-        }
-            public static string CityStateCountByIp(string IP)
-            {
-                //var url = "http://freegeoip.net/json/" + IP;
-                var url = "http://freegeoip.net/json/" + IP;
-                //string url = "http://api.ipstack.com/" + IP + "?access_key=[KEY]";
-                var request = System.Net.WebRequest.Create(url);
-
-                using (WebResponse wrs = request.GetResponse())
-                using (Stream stream = wrs.GetResponseStream())
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    string json = reader.ReadToEnd();
-                    //var obj = JObject.Parse(json);
-                    //string City = (string)obj["city"];
-                    //string Country = (string)obj["region_name"];
-                    //string CountryCode = (string)obj["country_code"];
-
-                    //return (CountryCode + " - " + Country + "," + City);
-                }
-
-                return "";
-
-            }
         public static void GetCountryByIP()
         {
-            IpInfo ipInfo = new IpInfo();
-
-            string info = new WebClient().DownloadString("http://ipinfo.io");
-
-            //JavaScriptSerializer jsonObject = new JavaScriptSerializer();
-            //ipInfo = jsonObject.Deserialize<IpInfo>(info);
-
-            //RegionInfo region = new RegionInfo(ipInfo.Country);
-
-            //Console.WriteLine(region.EnglishName);
-            //Console.ReadLine();
-
-            //{
-            //    "ip": "73.88.4.120",
-            //        "hostname": "c-73-88-4-120.hsd1.mn.comcast.net",
-            //        "city": "Minneapolis",
-            //        "region": "Minnesota",
-            //        "country": "US",
-            //        "loc": "44.9800,-93.2638",
-            //        "org": "AS7922 Comcast Cable Communications, LLC",
-            //        "postal": "55440",
-            //        "timezone": "America/Chicago",
-            //        "readme": "https://ipinfo.io/missingauth"
-            //}
-
-
-        }
-
-        public class IpInfo
-        {
-            //country
-            public string Country { get; set; }
+            IpInfo ipInfo = IpInfo.Get();
         }
 
         public static string GetLocalIPAddress()
@@ -147,32 +65,5 @@ namespace TestWpfApp
             return "";
         }
 
-    }
-
-    public class IpInfo
-    {
-        //[JsonProperty("ip")]
-        public string Ip { get; set; }
-
-        //[JsonProperty("hostname")]
-        public string Hostname { get; set; }
-
-        //[JsonProperty("city")]
-        public string City { get; set; }
-
-        //[JsonProperty("region")]
-        public string Region { get; set; }
-
-        //[JsonProperty("country")]
-        public string Country { get; set; }
-
-        //[JsonProperty("loc")]
-        public string Loc { get; set; }
-
-        //[JsonProperty("org")]
-        public string Org { get; set; }
-
-        //[JsonProperty("postal")]
-        public string Postal { get; set; }
     }
 }
