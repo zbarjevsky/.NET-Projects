5a54b9b197c2f4e3bcaae04f7cbe9cbee9f64571
mark.zbarjevsky <mark.zbarjevsky@ZBARJM1-TL1>
2020-05-23 21:54:33 -0500

    GradientProgressBar - improving implementation + Popup

--- Common.MkZ/Common.MkZ/WPF/GradientProgressBar.xaml.cs
+++ Common.MkZ/Common.MkZ/WPF/GradientProgressBar.xaml.cs
@@ -11,6 +11,7 @@ using System.IO;
 
 using System.Runtime.CompilerServices;
 using System.Windows.Shapes;
+using System.Windows.Media;
 
 namespace MZ.WPF
 {
@@ -31,7 +32,25 @@ namespace MZ.WPF
         }
 
         public static readonly DependencyProperty ValueProperty =
-            DependencyProperty.Register("Value", typeof(double), typeof(GradientProgressBar), new PropertyMetadata(default(double)));
+            DependencyProperty.Register("Value", typeof(double), typeof(GradientProgressBar), new PropertyMetadata(0.0));
+
+        public double Maximum
+        {
+            get { return (double)GetValue(MaximumProperty); }
+            set { SetValue(MaximumProperty, value); DrawTicks(); OnPropertyChanged(); }
+        }
+
+        public static readonly DependencyProperty MaximumProperty =
+            DependencyProperty.Register("Maximum", typeof(double), typeof(GradientProgressBar), new PropertyMetadata(180.0));
+
+        public SolidColorBrush TickColor
+        {
+            get { return (SolidColorBrush)GetValue(TickColorProperty); }
+            set { SetValue(TickColorProperty, value); DrawTicks(); OnPropertyChanged(); }
+        }
+
+        public static readonly DependencyProperty TickColorProperty =
+            DependencyProperty.Register("TickColor", typeof(SolidColorBrush), typeof(GradientProgressBar), new PropertyMetadata(Brushes.Green));
 
         public GradientProgressBar()
         {
@@ -41,6 +60,9 @@ namespace MZ.WPF
             m_Timer.Elapsed += m_Timer_Elapsed;
 
             DrawTicks();
+
+            //ProgressColor = Brushes.Pink;
+            //TickColor = Brushes.Navy;
         }
 
 		private void m_Timer_Elapsed(object sender, ElapsedEventArgs e)
@@ -52,16 +74,16 @@ namespace MZ.WPF
                 m_ElapsedTime += (now - m_LastTime);
             m_LastTime = now;
 
-            if (Max > 10)
+            if (Maximum > 10)
             {
-                Value = m_ElapsedTime.TotalSeconds % Max;
+                Value = m_ElapsedTime.TotalSeconds % Maximum;
             }
             else
             {
                 Value = 0;
             }
 
-            UpdateTooltip(Max - Value);
+            UpdateTooltip(Maximum - Value);
 
             m_Timer.Start();
         }
@@ -75,7 +97,7 @@ namespace MZ.WPF
                 string interval = "3 min"; // TimeSpan.FromSeconds(Settings.ProgressInterval).ToString(FMT);
                 string value = TimeSpan.FromSeconds(secondsLeft).ToString(FMT);
 
-                if (Max > 10)
+                if (Maximum > 10)
                 {
                     this.ToolTip = string.Format("Bell: {0}, Interval: {1:0} Time Left: {2:0}",
                         chk.IsChecked == true ? "On" : "Off", interval, value);
@@ -86,28 +108,6 @@ namespace MZ.WPF
                 }
             }));
         }
-
-        //private double _value = 30;
-        //public double Value
-        //{
-        //    get { return _value; }
-        //    set { _value = value;  OnPropertyChanged(); }
-        //}
-
-        private double _max = 180;
-		public double Max
-		{
-			get 
-			{ 
-				return _max; 
-			}
-			set 
-			{
-                _max = value;
-                DrawTicks();
-                OnPropertyChanged(); 
-			}
-		}
 
         #region INotifyPropertyChanged Members
 
@@ -121,10 +121,43 @@ namespace MZ.WPF
 
         #endregion
 
+        public class Theme
+        {
+            public int iBaseTickCount = 10;
+            public int iHalfTickCount = 30;
+            public int iFullTickCount = 60;
+
+            public static Theme GetClockTheme()
+            {
+                return new Theme()
+                {
+                    iBaseTickCount = 10,
+                    iHalfTickCount = 30,
+                    iFullTickCount = 60,
+                };
+            }
+
+            public static Theme GetBase100Theme()
+            {
+                return new Theme()
+                {
+                    iBaseTickCount = 10,
+                    iHalfTickCount = 50,
+                    iFullTickCount = 100,
+                };
+            }
+        }
+
+        public Theme ProgressTheme { get; set; } = Theme.GetClockTheme();
+
         private void DrawTicks()
         {
             _canvas.Children.Clear();
-            double line_count = Max;  //(Max / 30.0); //line per 30 sec
+
+            if (_canvas.ActualWidth == 0 || _canvas.ActualHeight == 0)
+                return;
+
+            double line_count = Maximum;  //(Max / 30.0); //line per 30 sec
             double line_offset = (_canvas.ActualWidth-3) / line_count;
             double smallDelta = _canvas.ActualHeight / 10;
             double bigDelta = _canvas.ActualHeight / 4;
@@ -134,17 +167,17 @@ namespace MZ.WPF
                 double thickness = -1;
                 double delta = bigDelta;
 
-                if (i % 10 == 0)
+                if (i % ProgressTheme.iBaseTickCount == 0)
                 {
                     thickness = 0.5;
                     delta = bigDelta;
                 }
-                if (i % 30 == 0)
+                if (i % ProgressTheme.iHalfTickCount == 0)
                 {
                     thickness = 2;
                     delta = bigDelta;
                 }
-                if (i % 60 == 0)
+                if (i % ProgressTheme.iFullTickCount == 0)
                 {
                     thickness = 2;
                     delta = smallDelta;
@@ -153,7 +186,7 @@ namespace MZ.WPF
                     continue;
 
                 Line line = new Line();
-                line.Stroke = System.Windows.Media.Brushes.Green;
+                line.Stroke = TickColor;
                 line.StrokeThickness = thickness;
                 line.X1 = 1 + i * line_offset;
                 line.X2 = line.X1;
--- Common.MkZ/Common.MkZ/WPF/PopupInfoWindow.xaml.cs
+++ Common.MkZ/Common.MkZ/WPF/PopupInfoWindow.xaml.cs
@@ -1,9 +1,9 @@
 ﻿using MZ.WPF;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace DesktopManagerUX.Controls
 {
@@ -20,6 +21,11 @@ namespace DesktopManagerUX.Controls
     /// </summary>
     public partial class PopupInfoWindow : Window
     {
+        private DispatcherTimer m_Timer = new DispatcherTimer(DispatcherPriority.Background);
+        private long _timer_count = 0;
+
+        public TimeSpan CloseTimeOut { get; set; } = TimeSpan.FromSeconds(4);
+
         public PopupInfoWindow()
         {
             InitializeComponent();
@@ -28,6 +34,40 @@ namespace DesktopManagerUX.Controls
 
             this.Left = 100;
             this.Top = 100;
+
+            m_Timer.Interval = TimeSpan.FromMilliseconds(100);
+            m_Timer.Tick += Timer_Tick;
+
+            m_Timer.Start();
+            _timer_count = 0;
+
+            _progress.ProgressTheme = GradientProgressBar.Theme.GetBase100Theme();
+            _progress.Maximum = 100;
+            _progress.TickColor = Brushes.Navy;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            double maxCount = CloseTimeOut.TotalSeconds / m_Timer.Interval.TotalSeconds;
+
+            m_Timer.Stop();
+            if (_timer_count++ > maxCount)
+            {
+                this.Close();
+                return; //do not start timer
+            }
+            m_Timer.Start();
+        }
+
+        private void Window_MouseEnter(object sender, MouseEventArgs e)
+        {
+            m_Timer.Stop();
+        }
+
+        private void Window_MouseLeave(object sender, MouseEventArgs e)
+        {
+            _timer_count = 0;
+            m_Timer.Start();
         }
     }
 }
