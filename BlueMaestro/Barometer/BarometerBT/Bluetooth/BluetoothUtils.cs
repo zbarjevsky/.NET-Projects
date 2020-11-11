c3230cd4e64429a0be2332518fca9c2b77c8c32f
mark.zbarjevsky <mark.zbarjevsky@ZBARJM1-TL1>
2020-11-11 14:41:00 -0600

    added BlueMaestroDevice and icon

--- BlueMaestro/Barometer/BarometerBT/Bluetooth/BluetoothUtils.cs
+++ BlueMaestro/Barometer/BarometerBT/Bluetooth/BluetoothUtils.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BarometerBT.BlueMaestro;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -6,33 +7,34 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Windows.Devices.Bluetooth.Advertisement;
+using Windows.Storage.Streams;
 
 namespace BarometerBT.Bluetooth
 {
     public class BluetoothUtils
     {
-        public void StartBluetoothSearch()
-        {
-            // Create Bluetooth Listener
-            var watcher = new BluetoothLEAdvertisementWatcher();
+        // Create Bluetooth Listener
+        private BluetoothLEAdvertisementWatcher _watcher = new BluetoothLEAdvertisementWatcher();
 
-            watcher.ScanningMode = BluetoothLEScanningMode.Active;
+        public void StartBluetoothSearch(int OutOfRangeTimeout = 50000, int SamplingInterval = 2000)
+        {
+            _watcher.ScanningMode = BluetoothLEScanningMode.Active;
 
             // Only activate the watcher when we're recieving values >= -80
-            watcher.SignalStrengthFilter.InRangeThresholdInDBm = -80;
+            _watcher.SignalStrengthFilter.InRangeThresholdInDBm = -80;
 
             // Stop watching if the value drops below -90 (user walked away)
-            watcher.SignalStrengthFilter.OutOfRangeThresholdInDBm = -90;
+            _watcher.SignalStrengthFilter.OutOfRangeThresholdInDBm = -90;
 
             // Register callback for when we see an advertisements
-            watcher.Received += OnAdvertisementReceived;
+            _watcher.Received += OnAdvertisementReceived;
 
             // Wait 5 seconds to make sure the device is really out of range
-            watcher.SignalStrengthFilter.OutOfRangeTimeout = TimeSpan.FromMilliseconds(5000);
-            watcher.SignalStrengthFilter.SamplingInterval = TimeSpan.FromMilliseconds(2000);
+            _watcher.SignalStrengthFilter.OutOfRangeTimeout = TimeSpan.FromMilliseconds(OutOfRangeTimeout);
+            _watcher.SignalStrengthFilter.SamplingInterval = TimeSpan.FromMilliseconds(SamplingInterval);
 
             // Starting watching for advertisements
-            watcher.Start();
+            _watcher.Start();
         }
 
         private void OnAdvertisementReceived(BluetoothLEAdvertisementWatcher watcher, BluetoothLEAdvertisementReceivedEventArgs eventArgs)
@@ -41,6 +43,41 @@ namespace BarometerBT.Bluetooth
             Debug.WriteLine(String.Format("Advertisement:"));
             Debug.WriteLine(String.Format("  BT_ADDR: {0}", eventArgs.BluetoothAddress));
             Debug.WriteLine(String.Format("  FR_NAME: {0}", eventArgs.Advertisement.LocalName));
+
+            try
+            {
+                if (eventArgs.Advertisement.ManufacturerData != null)
+                {
+                    var manufacturerSections = eventArgs.Advertisement.ManufacturerData;
+                    if (manufacturerSections.Count > 0)
+                    {
+                        foreach (BluetoothLEManufacturerData section in manufacturerSections)
+                        {
+                            // Only print the first one of the list
+                            var data = new byte[section.Data.Length];
+                            using (var reader = DataReader.FromBuffer(section.Data))
+                            {
+                                reader.ReadBytes(data);
+                            }
+
+                            //// Print the company ID + the raw data in hex format
+                            string manufacturerDataString = string.Format("0x{0}: {1}",
+                                section.CompanyId.ToString("X"),
+                                BitConverter.ToString(data));
+                            Debug.WriteLine("  COMPANY: " + manufacturerDataString);
+
+                            if(BlueMaestroDevice.IsManufacturerID(section.CompanyId))
+                            {
+                                BlueMaestroDevice dev = new BlueMaestroDevice(data);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                Debug.WriteLine("ERROR: "+err);
+            }
             Debug.WriteLine("");
         }
     }
