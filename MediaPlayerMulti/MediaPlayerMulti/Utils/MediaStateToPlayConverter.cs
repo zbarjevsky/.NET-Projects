687817b6efcb4cc4194555b999a0ae6dc91fcc1b
mark.zbarjevsky <mark.zbarjevsky@ZBARJM1-TL1>
2021-01-04 05:53:56 -0600

    MkZ.MediaPlayer - volume/mute toggle button

---
+++ MediaPlayerMulti/MediaPlayerMulti/Utils/MediaStateToPlayConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace MkZ.MediaPlayer.Utils
+{
+    [ValueConversion(typeof(MediaState), typeof(bool))]
+    public class MediaStateToPlayConverter : IValueConverter
+    {
+        #region IValueConverter Members
+        
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is MediaState state)
+                return state == MediaState.Play;
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+}
