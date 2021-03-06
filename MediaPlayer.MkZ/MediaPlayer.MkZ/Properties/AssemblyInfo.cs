2633745908d44ca526e53d34be766c45f0f728a0
Zbarjevsky, Mark <zbarjevsky@gmail.com>
2021-03-06 11:03:32 -0600

    MediaPlayer.MkZ - update version

--- MediaPlayer.MkZ/MediaPlayer.MkZ/Properties/AssemblyInfo.cs
+++ MediaPlayer.MkZ/MediaPlayer.MkZ/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.8.0")]
-[assembly: AssemblyFileVersion("1.0.8.0")]
+[assembly: AssemblyVersion("1.0.2021.0306")]
+[assembly: AssemblyFileVersion("1.0.2021.0306")]
 [assembly: NeutralResourcesLanguage("en-US")]
